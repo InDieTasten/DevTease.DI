19c4ee00cbad78531855a0a7109382ac53bcbf41
InDieTasten <indietasten@gmail.com>
2019-01-20 10:46:56 +0100

    Add project files.

---
+++ DevTease.DI.CollectionVsProvider/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevTease.DI.CollectionVsProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
